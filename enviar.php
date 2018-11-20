28cea882489731b3c38d49b02fac90e42035cab3
Jordan <i201412777@gmail.com>
2018-11-20 14:22:00 -0500

    Primer commit

---
+++ enviar.php
@@ -0,0 +1,11 @@
+<?php
+ $destino="i201412777@gmail.com";
+ $nombre=$_POST["nombre"];
+ $correo=$_POST["correo"];;
+ $telefono = $_POST["telefono"];
+ $mensaje=$_POST["mensaje"];
+ $contenido="Nombre: ".$nombre."\nCorreo: ".$correo."\nTelefono : ".$telefono."\nMensaje: ".$mensaje;
+ mail($destino,"Contacto", $contenido);
+ header("Location: index.html");
+
+?>
